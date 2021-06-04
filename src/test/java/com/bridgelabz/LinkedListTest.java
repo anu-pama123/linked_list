3ae22b3f1fee9f18f003199de241c28d95946181
anupama cv <anu.anupamacv@gmail.com>
2021-06-04 20:43:22 +0530

    ADDED: functionality to create a linked list of three numbers

--- src/test/java/com/bridgelabz/LinkedListTest.java
+++ src/test/java/com/bridgelabz/LinkedListTest.java
@@ -5,5 +5,14 @@ import org.junit.Test;
 
 public class LinkedListTest
 {
-
+    @Test
+    public void givenValueShouldAddInLinkedList()
+    {
+        MyLinkedList linkedList = new MyLinkedList();
+        linkedList.add(56);
+        linkedList.add(36);
+        linkedList.add(70);
+        int size=linkedList.size();
+        Assert.assertEquals(3, size);
+    }
 }
--- src/test/java/com/bridgelabz/NodeTest.java
+++ src/test/java/com/bridgelabz/NodeTest.java
@@ -5,5 +5,19 @@ import org.junit.Assert;
 
 public class NodeTest
 {
-
+    @Test
+    public void given3Node_shouldAddAtNextOfEachNode()
+    {
+        Node node1 = new Node();
+        node1.data = 1;
+        Node node2 = new Node();
+        node2.data = 2;
+        node1.next = node2;
+        Node node3 = new Node();
+        node3.data = 3;
+        node2.next = node3;
+        Integer node = null;
+        boolean status = node1.next.equals(node2) && node2.next.equals(node3);
+        Assert.assertTrue(status);
+    }
 }
